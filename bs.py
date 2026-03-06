083141db9ee21ccc7fe7a9a150b50d64d51179d9
asharbutt <49199106+asharbutt@users.noreply.github.com>
2026-03-07 00:46:02 +0500

    Create bs.py

---
+++ bs.py
@@ -0,0 +1,17 @@
+import numpy as np
+from scipy.stats import norm
+
+def bs_call(S, K, vol, T, r, q):
+    d_1 = (np.log(S / K) + (r - q + 0.5 * vol ** 2) * T) / (vol * np.sqrt(T))
+    d_2 = d_1 - vol * np.sqrt(T)
+    return S * norm.cdf(d_1) - K * np.exp(-r * T) * norm.cdf(d_2)
+
+def bs_put(S, K, vol, T, r, q):
+    d_1 = (np.log(S / K) + (r - q + 0.5 * vol ** 2) * T) / (vol * np.sqrt(T))
+    d_2 = d_1 - vol * np.sqrt(T)
+    return -S * norm.cdf(-d_1) + K * np.exp(-r * T) * norm.cdf(-d_2)
+
+def delta(S, K, vol, T, r, q):
+    d_1 = (np.log(S / K) + (r - q + 0.5 * vol ** 2) * T) / (vol * np.sqrt(T))
+    return norm.cdf(d_1)
+
